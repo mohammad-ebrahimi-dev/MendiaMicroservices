7e0f861a1f6c125294cb36fc7b763d9effe0207b
Mohammad Amin Ebrahimi <moiworktandis@gmail.com>
2025-12-27 16:41:21 +0330

    feature : Create Task and Group Entities

---
+++ TaskService/TaskService.Domain/Entity/Group.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskService.Domain.Entity
+{
+    public class Group
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Task> Tasks { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+    }
+}
---
+++ TaskService/TaskService.Domain/Entity/Task.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskService.Domain.Entity
+{
+    public class Task
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Guid GroupId { get; set; }
+        public Guid AssignedUserId { get; set; }
+        public Group Group { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModifiedDate{ get; set;}
+    }
+}
